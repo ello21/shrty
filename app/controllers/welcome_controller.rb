d96440e2e581d4736ba782a7358df5aaa5c9c3fc
Erica Lloyd <ello21@users.noreply.github.com>
2017-01-22 01:55:54 -0600

    Update home page to display short_links index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+	before_action :set_short_link, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @short_links = ShortLink.all
+  end
+
 end
