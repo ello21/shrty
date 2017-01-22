76eba587a367f0018736e789c8bc302d476e2abf
Erica Lloyd <ello21@users.noreply.github.com>
2017-01-22 04:25:56 -0600

    Add faker/default values to short_link factory

--- spec/factories/short_links.rb
+++ spec/factories/short_links.rb
@@ -1,7 +1,9 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :short_link do
-    user_short_key "MyString"
-    destination_url "MyText"
-    http_status 1
+    user_short_key { Faker::Internet.unique.password(1,10) }
+    destination_url { Faker::Internet.url }
+    http_status 301
   end
 end
