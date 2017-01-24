05740d7d597d8a7efc5418cdecc68aab6e7782a3
Erica Lloyd <ello21@users.noreply.github.com>
2017-01-24 16:33:28 -0600

    Remove text_message initialization from index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,6 @@ class WelcomeController < ApplicationController
   def index
     @short_links = ShortLink.all
     @short_link = ShortLink.new
-
-    @text_messages = TextMessage.all
-    @text_message = TextMessage.new
   end
 
 end
